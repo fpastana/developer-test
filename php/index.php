a7fadb8537ca7800d5ede1be231f4799dbd3fd61
Felipe <felipeapastana@gmail.com>
2015-07-31 01:23:29 -0300

    -- Teste Spider Produção

---
+++ php/index.php
@@ -0,0 +1,32 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pt" lang="pt-br">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta name="description" content="Consulta ao Sintegra da Secretaria de Estado da Fazenda do Espírito Santo"/>
+<meta name="keywords" content="sintegra, consulta, publica, cadastro, icms, es, sintegra-es, gov, espirito santo, br"/>
+<title>Consulta ao Sintegra</title>
+<h1 class="titulo">Entre com uma das op&ccedil;&otilde;es de consulta:</h1>
+</head>
+<body>
+<div id="site">
+<form name="formulario" method="post" action="resultado.php">
+<table border="0" class="entrada" >
+    <tr>
+    	<td colspan="2">&nbsp;</td>
+    </tr>
+    <tr>
+        <td align="right" class="dados">CNPJ:&nbsp;</td>
+        <td width="50%">
+        	<input type="text" name="num_cnpj" maxlength="18" size="19" />
+		</td>
+    </tr>
+    <tr>
+    	<td colspan="2">&nbsp;</td>
+    </tr>
+    <tr>
+        <td colspan="2" align="center"><br /><input type="submit" name="botao" value="Consultar" /></td>
+    </tr>
+</table>
+</form>
+</div>
+</body>
+</html>
